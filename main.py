42be946bb0abe1bd81cb5917e856265129c26cf6
kaittah <kaitlynhennacy@gmail.com>
2020-10-07 10:53:37 -0400

    added ai menu

--- main.py
+++ main.py
@@ -6,81 +6,32 @@ import pygame
 from menu import replay_menu
 from display import draw_text, redraw_window
 from gameplay import COLORS, GAMEDIMENSIONS, XY, initialize_board, get_impact, clear_selection, measure_fall, fall, locate, check_limits, edit_selection
+from play_game import play_game, WIN
+from play_ai_game import play_ai_game
 
 pygame.init()
 pygame.display.set_caption("Fruit Pop")
-
-FPS = 60
-MIN_CHAIN = 4
 FRUITS = list(COLORS.keys())
 XDIM, YDIM, WIDTH, HEIGHT, MARGIN, GUTTER, WINDOW_SIZE, GAMETIME = GAMEDIMENSIONS
-WIN = pygame.display.set_mode(WINDOW_SIZE)
-
-def play_game(board, startTime):
-    run = True
-    clock = pygame.time.Clock()
-    mouseDown = False
-    prevLoc = None
-    selection = []
-    colTotals = [0 for _ in range(XDIM)]
-    score = 0
-    #cause tiles to fall down when game starts
-    for r in range(YDIM):
-        for c in range(XDIM):
-            if random.random()>0.4:
-                selection.append(board[r][c])
-                colTotals[c]+=1
-    measure_fall(board, XY, colTotals)
-    board = fall(board, GAMEDIMENSIONS, colTotals, FRUITS)
-    selection.clear()
-    score = 0
-    endGame = False
-    fallStart = time.perf_counter()
-
-    while run:
-        if time.perf_counter() - startTime > GAMETIME:
-            run = False
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                endGame = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                mouseDown = False
-                if len(selection)>= MIN_CHAIN or selection and selection[0].fruit == 'bomb':
-                    board = clear_selection(board, GAMEDIMENSIONS, selection, FRUITS)
-                    score += len(selection)**2
-                else:
-                    for tile in selection:
-                        tile.selected = 0
-                selection.clear()
-                prevLoc = None
-                fallStart = time.perf_counter()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouseDown = True
-        if mouseDown:
-            xPos, yPos = pygame.mouse.get_pos()
-            loc = locate(xPos,yPos,board, GAMEDIMENSIONS)
-            if loc and check_limits(prevLoc, loc, board):
-                selection = edit_selection(selection,loc)
-                prevLoc = loc
-        redraw_window(WIN, GAMEDIMENSIONS, board, fallStart, startTime)
-        clock.tick(FPS)
-        pygame.display.update()
-    return score, endGame
 
 def main():
     run = True
     board = initialize_board(XY, FRUITS)
     redraw_window(WIN, GAMEDIMENSIONS, board)
-    draw_text(WIN, 'Click to Start', 30, (255,255,255), (WINDOW_SIZE[0])//2, (GUTTER)//2)
+    draw_text(WIN, 'Click here to Start', 50, (255,255,255), (WINDOW_SIZE[0])//2, (GUTTER)//2)
+    draw_text(WIN, 'Click here for Autopilot', 40, (255,255,255), (WINDOW_SIZE[0])//2, (GUTTER)//2 + 60)
     pygame.display.update()
     
     while run:
         for event in pygame.event.get():
+            xPos, yPos = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONUP:
-                run = replay_menu(board, play_game, draw_text, WIN, GAMEDIMENSIONS, FRUITS)
+                if yPos > GUTTER//2 + 40:
+                    run = replay_menu(board, play_ai_game, draw_text, WIN, GAMEDIMENSIONS, FRUITS)
+                else:
+                    run = replay_menu(board, play_game, draw_text, WIN, GAMEDIMENSIONS, FRUITS)
     pygame.quit()
     
 
---
+++ play_game.py
@@ -0,0 +1,65 @@
+import random
+import time
+
+import pygame
+
+from display import draw_text, redraw_window
+from gameplay import COLORS, GAMEDIMENSIONS, XY, initialize_board, get_impact, clear_selection, measure_fall, fall, locate, check_limits, edit_selection
+
+FPS = 60
+MIN_CHAIN = 4
+FRUITS = list(COLORS.keys())
+XDIM, YDIM, WIDTH, HEIGHT, MARGIN, GUTTER, WINDOW_SIZE, GAMETIME = GAMEDIMENSIONS
+WIN = pygame.display.set_mode(WINDOW_SIZE)
+
+def play_game(board, startTime):
+    run = True
+    clock = pygame.time.Clock()
+    mouseDown = False
+    prevLoc = None
+    selection = []
+    colTotals = [0 for _ in range(XDIM)]
+    score = 0
+    #cause tiles to fall down when game starts
+    for r in range(YDIM):
+        for c in range(XDIM):
+            if random.random()>0.4:
+                selection.append(board[r][c])
+                colTotals[c]+=1
+    measure_fall(board, XY, colTotals)
+    board = fall(board, GAMEDIMENSIONS, colTotals, FRUITS)
+    selection.clear()
+    score = 0
+    endGame = False
+    fallStart = time.perf_counter()
+
+    while run:
+        if time.perf_counter() - startTime > GAMETIME:
+            run = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                endGame = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouseDown = False
+                if len(selection)>= MIN_CHAIN or selection and selection[0].fruit == 'bomb':
+                    board = clear_selection(board, GAMEDIMENSIONS, selection, FRUITS)
+                    score += len(selection)**2
+                else:
+                    for tile in selection:
+                        tile.selected = 0
+                selection.clear()
+                prevLoc = None
+                fallStart = time.perf_counter()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouseDown = True
+        if mouseDown:
+            xPos, yPos = pygame.mouse.get_pos()
+            loc = locate(xPos,yPos,board, GAMEDIMENSIONS)
+            if loc and check_limits(prevLoc, loc, board):
+                selection = edit_selection(selection,loc)
+                prevLoc = loc
+        redraw_window(WIN, GAMEDIMENSIONS, board, fallStart, startTime)
+        clock.tick(FPS)
+        pygame.display.update()
+    return score, endGame
